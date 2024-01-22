d7eee5a59009d898686f7de3e86c4c8c0798af07
PRAWINBABUS <157091078+PRAWINBABUS@users.noreply.github.com>
2024-01-22 10:02:19 +0530

    Update object.js

--- object.js
+++ object.js
@@ -2,9 +2,39 @@
 var customerdetails={
     "Name":"prawin",
     "age":"20",
-    "phone.no":"9992882992",
+    "phone_no":"9992882992",
     
 }
 console.log(customerdetails)
 customerdetails["id"]="28439434"
 console.log(customerdetails)
+
+//second way
+
+var cusdetails={
+        
+}
+cusdetails['id']='tr28r7246'
+cusdetails['name']='prawin'
+cusdetails['age']='23'
+cusdetails['phone_no']='4872984724'
+console.log(cusdetails)
+
+//3rd way
+var kongu = new Object()
+kongu["fees"] = 100000
+kongu["accomdation"] = 'excellent'
+kongu["food"] = "average"
+kongu["hostelcount"] = 10
+console.log(kongu)
+console.log(kongu.food)
+console.log(kongu["hostelcount"])
+
+
+
+
+
+
+
+
+
