d4793f59609fefbc81b5d9388a2d188987403f6c
PRAWINBABUS <157091078+PRAWINBABUS@users.noreply.github.com>
2024-01-22 12:01:19 +0530

    Update set_map.js

--- set_map.js
+++ set_map.js
@@ -1,3 +1,5 @@
+//Set
+//example
 setExample= new Set("rcb","mi","csk") 
 console.log(setExample)
 
@@ -11,3 +13,27 @@ for(i of setExample)
 {
     console.log(i)
 }
+
+//map
+mapEg=new Map([
+    ["id","21dnfw21"],
+    ["name","prawin"]
+    ])
+console.log(mapEg)
+for(i in mapEg)
+{
+    console.log(i)
+}
+for(i of mapEg)
+{
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
