4a0f8c75f14432823cb1bc0d5e887b918ec28e4b
PRAWINBABUS <157091078+PRAWINBABUS@users.noreply.github.com>
2024-01-22 09:56:02 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -14,3 +14,10 @@ cusdetails['name']='prawin'
 cusdetails['age']='23'
 cusdetails['phone_no']='4872984724'
 console.log(cusdetails)
+
+var kongu = new Object()
+kongu["fees"] = 100000
+kongu["accomdation"] = 'excellent'
+kongu["food"] = "average"
+kongu["hostelcount"] = 10
+console.log(kongu)
