4581b15a9dd067fc09590efb794270ebd4b718c6
PRAWINBABUS <157091078+PRAWINBABUS@users.noreply.github.com>
2024-01-22 09:47:31 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -4,3 +4,13 @@ let b = 'variable'
 console.log("Variable of b",b)
 const d = 100
 console.log("Printed result",d)
+//second way
+
+var cusdetails={
+        
+}
+cusdetails['id']='tr28r7246'
+cusdetails['name']='prawin'
+cusdetails['age']='23'
+cusdetails['phone_no']='4872984724'
+console.log(cusdetails)
