8c73a719e908d92dd54cf3098d2c93f3ba71ff40
PRAWINBABUS <157091078+PRAWINBABUS@users.noreply.github.com>
2024-01-22 09:41:44 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,10 @@
+// how to declare objects
+var customerdetails={
+    "Name":"prawin",
+    "age":"20",
+    "phone.no":"9992882992",
+    
+}
+console.log(customerdetails)
+customerdetails["id"]="28439434"
+console.log(customerdetails)
